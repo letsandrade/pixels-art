db62788c7ea34e95bf2cf1fe6ad215ae45ac82e6
leticiaandrade <leticia933@gmail.com>
2021-11-23 01:10:04 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function criaGrid(params) {
-let pixelgrid = document.querySelector(".pixel")
-    for (i = 0; i < 5; i = i+=1) {
-        document.createElement
-    }
+const black = document.getElementById("color-black")
+const blue = document.getElementById("color-blue")
+const purple = document.getElementById("color-purple")
+const pink = document.getElementById("color-pink")
+
+let selectedColor = black;
+
+//console.log(document.getElementsByTagName(div))
+const elementBody = document.getElementById('body')
+elementBody.appendChild(document.createElement('section'));
+const pixelBoard = document.getElementById('pixel-board')
+
+for (let i = 0; i < 25; i += 1) {
+    const elementDiv = document.createElement('div'); 
+    elementDiv.className = "pixel"    
+    pixelBoard.appendChild(elementDiv)
+    
+
 }
+// kudos: mislaine presidente, esdras, leo carvalho, fernando cabral
+
+//function criaGrid() {
+   // document.getElementById("#pixel-board").appendChild(div)
+//}
