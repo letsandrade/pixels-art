6e603bf9905e3ddf97a6ff2373d615f539b1146a
leticiaandrade <leticia933@gmail.com>
2021-11-29 02:19:43 -0300

    requisito 6

--- script.js
+++ script.js
@@ -2,17 +2,31 @@ const black = document.getElementById("color-black")
 const blue = document.getElementById("color-blue")
 const purple = document.getElementById("color-purple")
 const pink = document.getElementById("color-pink")
-const colorPalette = document.getElementById('color-palette')
+const colorPalette = document.getElementById('color-palette').children
 
-let selectedColor = black;
-colorPalette.addEventListener('click', selectColor);
-
-function selectColor(event) {
-
-    for (let i = 0; i < colorPalette.length; i += 1) {
-        
-    }
+//tornar paleta de cores clicavel
+for (let counter = 0; counter < colorPalette.length; counter += 1) {
+    colorPalette[counter].addEventListener('click', selectColor);
 }
+
+//selecionar e capturar a cor
+function selectColor(event) {
+    for (let c = 0; c < colorPalette.length; c += 1) {
+        if (colorPalette[c].classList.contains('selected')) {
+            colorPalette[c].classList.remove('selected')    
+        }
+    }
+    event.target.classList.add('selected');
+    let selected = '';
+}
+
+
+function selectedColor() {
+    var elem = document.getElementById("test");
+    var theCSSprop = window.getComputedStyle(elem, null).getPropertyValue("background-color");
+    document.getElementById("demo").innerHTML = theCSSprop;
+  }
+
 
 //console.log(document.getElementsByTagName(div))
 const elementBody = document.getElementById('body')
@@ -21,8 +35,13 @@ const pixelBoard = document.getElementById('pixel-board')
 
 for (let i = 0; i < 25; i += 1) {
     const elementDiv = document.createElement('div'); 
-    elementDiv.className = "pixel"    
+    elementDiv.className = "pixel"
+    elementDiv.addEventListener('click', paintPixel);    
     pixelBoard.appendChild(elementDiv);
+}
+
+function paintPixel(event) {
+    
 }
 // kudos: mislaine presidente, esdras, leo carvalho, fernando cabral
 
